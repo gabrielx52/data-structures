ea7557c69525bd77ec60aa7b458f2a86de667457
Marco Zangari <mjzangari@gmail.com>
2017-10-23 21:30:01 -0700

    Test Pop Wrote test for pop method in the LinkedList class object. Ran in py.test and works

--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -11,3 +11,11 @@ def test_node_instance():
     """Test new instance of Node obj."""
     from linked_list import Node
     assert type(Node()) == Node
+
+
+def test_pop():
+    """Test linked list pop method."""
+    from linked_list import LinkedList
+    ll = LinkedList()
+    ll.push('val')
+    assert ll.pop() == 'val'
