9b4d0387cd0fd42fc22a1c5474d70ab32d20e56c
Marco Zangari <mjzangari@gmail.com>
2017-10-26 16:59:28 -0700

    Doubly linked list Node wrote the test for the instance of the node and testing for the instance of the node object.

---
+++ src/doubly_linked_list.py
@@ -0,0 +1,11 @@
+"""Doubly linked list data structures."""
+
+class Node(object):
+    """Make the node object."""
+
+    def __init__(self):
+        """Make node object class."""
+        self.data = None
+        self.next = None
+        self.prev = None
+
