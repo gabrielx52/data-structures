1663959f83d56bc15b54fbf5dbc9e3ba11934546
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-25 19:10:43 -0700

    Added __len__ function. Added __len__ method to return stack length with len() function.

--- src/stack.py
+++ src/stack.py
@@ -8,11 +8,19 @@ class Stack(object):
     def __init__(self, iterable=()):
         """Init new stack instance."""
         self.ll = LinkedList(iterable)
+        self._count = 0
 
     def push(self, val):
         """Push new item on stack."""
         self.ll.push(val)
+        self._count += 1
 
     def pop(self):
         """Pop last-in item on stack."""
-        return self.ll.pop()
+        pop_node = self.ll.pop()
+        self._count -= 1
+        return pop_node
+
+    def __len__(self):
+        """Return size of stack with len() function."""
+        return self._count
--- src/test_stack.py
+++ src/test_stack.py
@@ -7,10 +7,20 @@ def test_stack_type():
     stck = Stack()
     assert type(stck) == Stack
 
+
 def test_push_and_pop():
-    """Test the push and pop method of stack."""
+    """Test the push and pop method on stack."""
     from stack import Stack
     stck = Stack()
     stck.push('one')
     stck.push('two')
     assert stck.pop() == 'two'
+
+
+def test_stack_len():
+    """Test the len function on stack."""
+    from stack import Stack
+    stck = Stack()
+    stck.push('one')
+    stck.push('two')
+    assert len(stck) == 2
