d59f8f53ffdcd14c61491500c6ef2794c3061648
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-26 16:09:30 -0700

    Refactored Stack class. Added error handling and refactored len function for stack class.

--- src/stack.py
+++ src/stack.py
@@ -7,20 +7,19 @@ class Stack(object):
 
     def __init__(self, iterable=()):
         """Init new stack instance."""
-        self.ll = LinkedList(iterable)
-        self._count = 0
+        self._ll = LinkedList(iterable)
 
     def push(self, val):
         """Push new item on stack."""
-        self.ll.push(val)
-        self._count += 1
+        self._ll.push(val)
 
     def pop(self):
         """Pop last-in item on stack."""
-        pop_node = self.ll.pop()
-        self._count -= 1
-        return pop_node
+        try:
+            return self._ll.pop()
+        except IndexError:
+            raise IndexError('Cannot pop from an empty stack.')
 
     def __len__(self):
         """Return size of stack with len() function."""
-        return self._count
+        return len(self._ll)
