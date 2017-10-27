966775fdc6ed870f7f9923dfe47f54d8f5c27bf7
Marco Zangari <mjzangari@gmail.com>
2017-10-26 18:05:26 -0700

    Added Push Added push function to the doubly linked list module and tests. The tests passed on pytest watch.

--- src/doubly_linked_list.py
+++ src/doubly_linked_list.py
@@ -19,3 +19,17 @@ class DoublyLinkedList(object):
         self.head = None
         self.tail = None
         self._len = 0
+
+    def push(self, val):
+        """Push doubly linked list."""
+        new_node = Node()
+        new_node.data = val
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+            self._len += 1
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+            self._len += 1
--- src/test_doubly_linked_list.py
+++ src/test_doubly_linked_list.py
@@ -11,3 +11,13 @@ def test_dll_instance():
     """Test new instance of doubly linked list."""
     from doubly_linked_list import DoublyLinkedList
     assert type(DoublyLinkedList()) == DoublyLinkedList
+
+
+def test_dll_push():
+    """Test the push method on doubly linked list."""
+    from doubly_linked_list import DoublyLinkedList
+    dll = DoublyLinkedList()
+    dll.push('one')
+    dll.push('two')
+    dll.push('three')
+    assert dll._len == 3
