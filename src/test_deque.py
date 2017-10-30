1877034819b8b76313bbbe3552a402c6fce1d731
Marco Zangari <mjzangari@gmail.com>
2017-10-30 14:06:43 -0700

    added indentation

--- src/test_deque.py
+++ src/test_deque.py
@@ -25,8 +25,8 @@ def test_appendleft():
 
 
 def test_pop():
-     """Test adding to front (appendleft) on deque object."""
+     """Test adding to tail (appendleft) on deque object."""
     from deque import Deque
     dq = Deque()
     dq.append('one')
-    assert dq.pop('one') = 'one'
+    assert dq.pop('one') == 'one'
