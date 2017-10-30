2d8e0e9075caed4bf32b08e09efaa1962a42bd33
Marco Zangari <mjzangari@gmail.com>
2017-10-30 13:44:19 -0700

    Append Test: Test the adding to the end of the deque list

--- src/test_deque.py
+++ src/test_deque.py
@@ -6,3 +6,10 @@ def test_deque_object_type():
     from deque import Deque
     dq = Deque()
     assert type(dq) == Deque
+
+
+def test_append():
+    """Test adding to end (append) on deque object."""
+    from deque import Deque
+    dq = Deque()
+    assert dq.append('one') == 'one'
