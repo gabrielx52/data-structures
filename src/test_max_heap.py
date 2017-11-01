c3f75175394eff9605cb539dfef6e2b4ed056205
Marco Zangari <mjzangari@gmail.com>
2017-10-31 17:05:07 -0700

    Test Push Testing the length of the list when one value has been pushed into the initialized empty list

--- src/test_max_heap.py
+++ src/test_max_heap.py
@@ -6,3 +6,11 @@ def test_max_heap():
     from max_heap import MaxHeap
     mh = MaxHeap()
     assert isinstance(mh, MaxHeap)
+
+
+def test_max_heap_push():
+    """Test max heap push: add data node to the list."""
+    from max_heap import MaxHeap
+    mh = MaxHeap()
+    mh.push('one')
+    assert len(mh._heap) == 1
