0882f9d6e99bc56e005544306aab1e6f2bcff8ba
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-01 13:48:47 -0700

    Added test for parent method. Test if MaxHeap parent method will return the correct parent index of data in the heap.

--- src/test_max_heap.py
+++ src/test_max_heap.py
@@ -23,4 +23,11 @@ def test_max_heap_binary_structure():
     mh.push(1)
     mh.push(8)
     mh.push(11)
-    assert mh._heap == [11, 8, 1]
+    assert mh._heap == [11, 1, 8]
+
+
+def test_parent_for_item_in_heap():
+    """Test the parent method returns correct parent index."""
+    from max_heap import MaxHeap
+    mh = MaxHeap()
+    assert mh.parent(9) == 4
