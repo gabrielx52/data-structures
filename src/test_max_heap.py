1775adb86adc833c5c81fc2335ed29c128ea85eb
Marco Zangari <mjzangari@gmail.com>
2017-10-31 17:13:20 -0700

    Added Test Functionality of checking that the list is in order of highest to lowest value

--- src/test_max_heap.py
+++ src/test_max_heap.py
@@ -12,5 +12,15 @@ def test_max_heap_push():
     """Test max heap push: add data node to the list."""
     from max_heap import MaxHeap
     mh = MaxHeap()
-    mh.push('one')
+    mh.push(1)
     assert len(mh._heap) == 1
+
+
+def test_max_heap_binary_structure():
+    """Test max heap push: add data node to the list."""
+    from max_heap import MaxHeap
+    mh = MaxHeap()
+    mh.push(1)
+    mh.push(8)
+    mh.push(11)
+    assert mh._heap == [11, 8, 1]
