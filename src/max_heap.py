cb581933b896b65fa4c6453a726ed286d7032388
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-31 17:00:16 -0700

    Created MaxHeap object. Max heap object init created, will init with empty heap.

--- src/max_heap.py
+++ src/max_heap.py
@@ -1,2 +1,9 @@
 """Implement max heap abstract structure."""
 
+
+class MaxHeap(object):
+    """Binary max heap data structure."""
+
+    def __init__(self):
+        """Max heap init."""
+        _heap = []
