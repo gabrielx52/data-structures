a19894a5c3e56394a117f88811eb72e1f7ae9f8f
Marco Zangari <mjzangari@gmail.com>
2017-11-01 13:37:22 -0700

    Sort Child Parent Added functions for sorting. Created functions for creating parent, child left and right. It works.

--- src/max_heap.py
+++ src/max_heap.py
@@ -10,4 +10,35 @@ class MaxHeap(object):
 
     def push(self, val):
         """Add data to heap."""
-        self._heap.append(val)
+        if not self._heap:
+            self._heap.append(val)
+        else:
+            self._heap.append(val)
+            self.sort(len(self._heap) - 1, val)
+
+    def parent(self, i):
+        """Return index of parent node."""
+        return (i - 1) // 2
+
+    def left_child(self, i):
+        """Return index of left child node."""
+        return 2 * i + 1
+
+    def right_child(self, i):
+        """Return index of right child node."""
+        return 2 * i + 1
+
+    def sort(self, i, val):
+        """Sort the heap."""
+        n_idx = i
+        p_idx = self.parent(i)
+        if self._heap[n_idx] > self._heap[p_idx]:
+            self._heap[n_idx] = self._heap[p_idx]
+            self._heap[n_idx] = self._heap[p_idx]
+            self._heap[p_idx] = val
+            if p_idx:
+                self.sort(p_idx, val)
+            else:
+                return
+        else:
+            return
