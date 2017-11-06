b14373791004bd744b737ffc3824272179355fcd
Marco Zangari <mjzangari@gmail.com>
2017-11-06 13:28:51 -0800

    Test for insert

--- src/test_priority_que.py
+++ src/test_priority_que.py
@@ -2,7 +2,15 @@
 
 
 def test_priority_que():
-    """."""
+    """Test priority_que produces an instance of Priorityq."""
     from priority_que import Priorityq
     pq = Priorityq()
     assert isinstance(pq, Priorityq)
+
+
+def test_insert():
+    """."""
+    from priority_que import Priorityq
+    pq = Priorityq()
+    pq.insert(8)
+    assert pq._q == {0: [8]}
