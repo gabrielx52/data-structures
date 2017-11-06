9cfbe5edf9bb8efc2c19428e8f151e8c59c05df9
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-06 08:12:20 -0800

    Inital graph commit.

---
+++ src/graph_1.py
@@ -0,0 +1,40 @@
+"""."""
+
+
+class Graph(object):
+    """."""
+
+    def __init__(self):
+        """."""
+        self._g = {}
+
+    def nodes(self):
+        """Return a list of all nodes in the graph."""
+        return self._g.keys()
+
+    def edges(self):
+        """Return a list of all edges in the graph."""
+        vals = list(self._q.values())
+        edges = [item for sublist in vals for item in sublist]
+        return edges
+
+    def add_node(self, val):
+        """Add a new node to the graph."""
+
+    def add_edge(self, val1, val2):
+        """. Add a new edge to the graph."""
+
+    def del_node(self, val):
+        """Delete the node containing ‘val’."""
+
+    def del_edge(self, val1, val2):
+        """Delete an edge from the graph."""
+
+    def has_node(self, val):
+        """True if node present in graph, else False."""
+
+    def neighbors(self, val):
+        """Return the list of all node's neighbor nodes."""
+
+    def adjacent(self, val1, val2):
+        """True if edge connecting val1 and val2, else False."""
