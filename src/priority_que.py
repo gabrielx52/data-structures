a4f512637cd3ed1585b0589a7341bb2d73364acc
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-06 08:25:20 -0800

    Added insert method. Insert method will add new item to queue, has optional weight param that determines priority.

--- src/priority_que.py
+++ src/priority_que.py
@@ -1,15 +1,19 @@
-"""."""
+"""Priority queue data structure."""
 
 
 class Priorityq(object):
-    """."""
+    """Priority queue class."""
 
     def __init__(self):
-        """."""
+        """Priority queue constructor method."""
         self._q = {}
 
     def insert(self, val, weight=0):
-        """."""
+        """Insert value into priority queue."""
+        try:
+            self._q[weight].append(val)
+        except:
+            self._q[weight] = [val]
 
     def peek(self):
         """."""
