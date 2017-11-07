f80bf29ff0d1ff2272508262fb0bc830789db700
Marco Zangari <mjzangari@gmail.com>
2017-11-07 14:02:48 -0800

    test for add edges added, about to write function for graph.

--- src/test_graph_1.py
+++ src/test_graph_1.py
@@ -3,19 +3,26 @@ import pytest
 
 
 def test_init_isinstance_dict():
-    """."""
+    """Test is g an instance of Graph."""
     from graph_1 import Graph
     g = Graph()
     assert isinstance(g, Graph)
 
 
 def test_nodes_returns_list():
-    """."""
+    """Test."""
     from graph_1 import Graph
     g = Graph()
     g_list = g.nodes()
     assert g_list == []
 
+
+def test_add_edges_return_val():
+    """."""
+    from graph_1 import Graph
+    g = Graph()
+    g.add_edge(2, 3)
+    assert g._g == {2:[3], 3:[]}
 
 
 def test_nodes_return_added_node_with_empty_list():
@@ -24,4 +31,3 @@ def test_nodes_return_added_node_with_empty_list():
     g = Graph()
     g.add_node(3)
     assert g._g[3] == []
-
