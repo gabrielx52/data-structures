2257daa3087f673641805026b30dc6dcfc1897ed
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-07 14:54:42 -0800

    Added test for edges method. Test that edges method returns a list of edge tuples.

--- src/test_graph_1.py
+++ src/test_graph_1.py
@@ -2,6 +2,17 @@
 import pytest
 
 
+@pytest.fixture
+def g3_fixt():
+    """Graph test fixture with 3 nodes."""
+    from graph_1 import Graph
+    g = Graph()
+    g.add_node(1)
+    g.add_node(2)
+    g.add_node(3)
+    return g
+
+
 def test_init_isinstance_dict():
     """Test is g an instance of Graph."""
     from graph_1 import Graph
@@ -18,16 +29,24 @@ def test_nodes_returns_list():
 
 
 def test_add_edges_return_val():
-    """."""
+    """Test add edge adds an edge to val1 node."""
     from graph_1 import Graph
     g = Graph()
     g.add_edge(2, 3)
-    assert g._g == {2:[3], 3:[]}
+    assert g._g == {2: [3], 3: []}
 
 
 def test_nodes_return_added_node_with_empty_list():
-    """."""
+    """Test add node creates empty dict as starting value."""
     from graph_1 import Graph
     g = Graph()
     g.add_node(3)
     assert g._g[3] == []
+
+
+def test_edges_method_returns_list_of_tuples_of_edges(g3_fixt):
+    """Test that edges method returns list of edge tuples."""
+    g3_fixt.add_edge(1, 2)
+    g3_fixt.add_edge(1, 3)
+    g3_fixt.add_edge(2, 3)
+    assert g3_fixt.edges() == [(1, 2), (1, 3), (2, 3)]
