e2cebc737fd22c98962a68ea29339d4d3990ecbf
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-20 21:11:27 -0800

    Added insert method to binary search tree.

--- src/bst.py
+++ src/bst.py
@@ -1,6 +1,16 @@
 """Binary search tree."""
 
 
+class Node(object):
+    """BST Node object class."""
+
+    def __init__(self, val):
+        """Constructor method for BST Node."""
+        self.val = val
+        self.left = None
+        self.right = None
+
+
 class BST(object):
     """Binary search tree object class."""
 
@@ -9,9 +19,33 @@ class BST(object):
         self.iter = iterable
         self.root = None
         self.size = 0
+        self.depth = 0
+        self.balance = 0
 
     def insert(self, val):
         """Insert value into BST."""
+        if self.root is None:
+            self.root = Node(val)
+            self.size += 1
+            return
+        current = self.root
+        while True:
+            if val > current.val:
+                if current.right:
+                    current = current.right
+                else:
+                    current.right = Node(val)
+                    self.size += 1
+                    return
+            if val < current.val:
+                if current.left:
+                    current = current.left
+                else:
+                    current.left = Node(val)
+                    self.size += 1
+                    return
+            if val == current.val:
+                return
 
     def search(self, val):
         """Return node containing val."""
@@ -22,9 +56,11 @@ class BST(object):
 
     def depth(self):
         """Return depth of BST."""
+        return self.depth
 
     def contains(self, val):
         """Return True/False if val is in BST."""
 
     def balance(self):
         """Return True/False if BST is balanced."""
+        return self.balance == 0
