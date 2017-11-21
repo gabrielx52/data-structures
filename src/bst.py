6cbbb024702f7b12a3e653256de82e0409d4f12e
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-20 19:22:46 -0800

    Initial commit of binary search tree.

---
+++ src/bst.py
@@ -0,0 +1,30 @@
+"""Binary search tree."""
+
+
+class BST(object):
+    """Binary search tree object class."""
+
+    def __init__(self, iterable=None):
+        """Constructor method for BST."""
+        self.iter = iterable
+        self.root = None
+        self.size = 0
+
+    def insert(self, val):
+        """Insert value into BST."""
+
+    def search(self, val):
+        """Return node containing val."""
+
+    def size(self):
+        """Return size of BST."""
+        return self.size
+
+    def depth(self):
+        """Return depth of BST."""
+
+    def contains(self, val):
+        """Return True/False if val is in BST."""
+
+    def balance(self):
+        """Return True/False if BST is balanced."""
