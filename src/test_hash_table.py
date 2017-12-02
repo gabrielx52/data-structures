06bcd2f1b158d45969feaaa308a677e594d20850
Marco Zangari <mjzangari@gmail.com>
2017-12-02 15:28:20 -0800

    hash test for size

--- src/test_hash_table.py
+++ src/test_hash_table.py
@@ -4,7 +4,14 @@ import pytest
 
 
 def test_hash_table():
-    """test is HashTable an instance."""
+    """Test HashTable an instance."""
     from hash_table import HashTable
     h = HashTable(11, 1)
     assert isinstance(h, HashTable)
+
+
+def test_hash_table_size():
+    """Test HashTable has size 1."""
+    from hash_table import HashTable
+    h = HashTable(11, 1)
+    assert h.size == 11
