fca4f8747d5e40e8220d8c2f3c8af1393a4d62b3
Marco Zangari <mjzangari@gmail.com>
2017-12-03 14:15:05 -0800

    initial commmit with trie module methods and test trie module.

---
+++ src/trie.py
@@ -0,0 +1,14 @@
+"""Implement trie tree data structure."""
+
+
+def insert(self, string):
+    """Insert input string into trie; ignore, if char already present."""
+
+def contains(self, string):
+    """Return True if in string; False if not."""
+
+def size(self):
+    """Return total number of words in trie; 0 if empty."""
+
+def remove(self, string):
+    """Remove given string from trie; raise exception if not present."""
