f3b534232725e7ac614c854e20f7321bea70061e
Marco Zangari <mjzangari@gmail.com>
2017-12-06 20:59:30 -0800

    not working. breaks out of loop before sorted

--- src/bubble.py
+++ src/bubble.py
@@ -3,12 +3,18 @@
 
 def bubble_sort(alist):
     """Implement a bubble sort."""
-    while True:
-        for i in range(len(alist)):
+    data_list = len(alist)
+    potato = True
+    while potato:
+        potato = False
+        for i in range(len(alist - 1)):
             if alist[i - 1] > alist[i]:
                 temp = alist[i]
                 alist[i] = alist[i - 1]
                 alist[i - 1] = temp
+
+        potato = True
+    break
     return alist
 
 # if __name__ == ‘__main__’:
