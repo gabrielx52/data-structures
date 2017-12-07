1e6965bbfb87a5efd8f495d05197d31a70e04a1e
Marco Zangari <mjzangari@gmail.com>
2017-12-06 22:01:38 -0800

    fixed the sort. and prints to console

--- src/bubble.py
+++ src/bubble.py
@@ -3,18 +3,18 @@
 
 def bubble_sort(alist):
     """Implement a bubble sort."""
-    data_list = len(alist)
     potato = True
     while potato:
         potato = False
-        for i in range(len(alist - 1)):
-            if alist[i - 1] > alist[i]:
-                temp = alist[i]
-                alist[i] = alist[i - 1]
-                alist[i - 1] = temp
-
-        potato = True
-    break
+        for idx in range(len(alist) - 1):
+            if alist[idx] > alist[idx + 1]:
+                temp = alist[idx]
+                alist[idx] = alist[idx + 1]
+                alist[idx + 1] = temp
+                potato = True
     return alist
 
-# if __name__ == ‘__main__’:
+if __name__ == '__main__':
+    print('A list: [4, 3, 7, 6] using bubble sort')
+    alist = [4, 3, 7, 6]
+    print(bubble_sort(alist))
