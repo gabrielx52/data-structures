4e033ce4788285865188fcca77718df41694fe1e
gabrielx52 <gabriel.meringolo@gmail.com>
2017-12-07 20:15:49 -0800

    Added coveragerc.

--- src/insert.py
+++ src/insert.py
@@ -3,16 +3,19 @@
 
 def insert_sort(l):
     """Sort a list of numbers by insert sort."""
-    for i in range(len(l) - 1):
-        for j in range(i + 1, 0, -1):
-            if l[j] < l[j - 1]:
-                tmp = l[j]
-                l[j] = l[j - 1]
-                l[j - 1] = tmp
-    return l
+    if isinstance(l, list):
+        for i in range(len(l) - 1):
+            for j in range(i + 1, 0, -1):
+                if l[j] < l[j - 1]:
+                    tmp = l[j]
+                    l[j] = l[j - 1]
+                    l[j - 1] = tmp
+        return l
+    else:
+        raise TypeError('Input type must be a list.')
 
 
-if __name__ == '__main__':  # pragama: no cover
+if __name__ == '__main__':
     import timeit as ti
     list1 = [1, 2, 4, 6]
     list2 = [9, 8, 7, 6, 5, 4, 3, 2, 1]
