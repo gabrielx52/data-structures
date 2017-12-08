62f424f12a8787be0438eb92dc0f51cbad5902eb
gabrielx52 <gabriel.meringolo@gmail.com>
2017-12-07 20:00:13 -0800

    Updated README with big o.

--- src/insert.py
+++ src/insert.py
@@ -10,3 +10,30 @@ def insert_sort(l):
                 l[j] = l[j - 1]
                 l[j - 1] = tmp
     return l
+
+
+if __name__ == '__main__':  # pragama: no cover
+    import timeit as ti
+    list1 = [1, 2, 4, 6]
+    list2 = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    list3 = list(range(100, 0, -1))
+
+    time_1 = ti.timeit("insert_sort(list1)",
+                       setup="from __main__ import list1, insert_sort")
+    time_2 = ti.timeit("insert_sort(list2)",
+                       setup="from __main__ import list2, insert_sort")
+    time_3 = ti.timeit("insert_sort(list3)",
+                       setup="from __main__ import list3, insert_sort")
+    print(f"""
+Insert sort sorts a list by stepping through it, swapping items
+depending on value until no more swaps can be made.
+
+Input:[1, 2, 4, 6]
+Sort time: {time_1}
+
+Input:[9, 8, 7, 6, 5, 4, 3, 2, 1]
+Sort time: {time_2}
+
+Input:list(range(100, 0, -1))
+Sort time: {time_3}
+        """)
